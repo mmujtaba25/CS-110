cf030695a1c091ec884ecb9c2f80a46382de00d4
mmujtaba25 <229266859+mmujtaba25@users.noreply.github.com>
2025-11-19 18:41:57 +0500

    Add submodule example

--- setup.sh
+++ setup.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 set -e
 
+# Adding a submodule for demonstration purposes
+# git submodule add LINK path/to/submodule
+
 # Initialize and update all git submodules
 git submodule update --init --recursive
